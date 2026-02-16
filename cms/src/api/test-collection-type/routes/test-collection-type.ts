8fbbd1c597b7f8d7b5bb356dbb58cf25f34efdfa
Maxim Rychkov <rychkov03@yandex.ru>
2026-02-16 13:33:06 +0500

    test(cms): add test collection

---
+++ cms/src/api/test-collection-type/routes/test-collection-type.ts
@@ -0,0 +1,7 @@
+/**
+ * test-collection-type router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::test-collection-type.test-collection-type');
