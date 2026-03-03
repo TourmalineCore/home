98f4824fb5769ec5586f2e28da91bae92d7c1c0a
Dmitriy Myakotin <75628188+MDI74@users.noreply.github.com>
2026-03-03 10:32:52 +0500

    fix: #76: fix lint

--- ui/components/redesign/HeaderRedesign/HeaderRedesign.tsx
+++ ui/components/redesign/HeaderRedesign/HeaderRedesign.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import Image from "next/image"; 
+import Image from "next/image";
 import { useEffect, useState } from "react";
 import clsx from "clsx";
 import { useRouter } from "next/router";
