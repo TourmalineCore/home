673aa5ffdeef96ce2463852476b6938776b04f0f
Ekaterina <echekina@gmail.com>
2026-04-13 16:12:20 +0500

    chore: #74: test

--- ui/components/chelzoo/ChelzooHero/ChelzooHero.tsx
+++ ui/components/chelzoo/ChelzooHero/ChelzooHero.tsx
@@ -18,7 +18,7 @@ export function ChelzooHero({
 
   return (
     <section
-      className="chelzoo-hero"
+      className="chelzoo-hero test-class"
       data-testid="chelzoo-hero"
     >
       <div className="chelzoo-hero__decor chelzoo-hero__decor--left" />
