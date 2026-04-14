2712f4cf98b86735c595b81c8ebce09858748bb3
Ekaterina <echekina@gmail.com>
2026-04-14 15:20:24 +0500

    style(ui): #74: component styling

--- ui/components/chelzoo/ChelzooDevDesign/ChelzooDevDesign.tsx
+++ ui/components/chelzoo/ChelzooDevDesign/ChelzooDevDesign.tsx
@@ -1,9 +1,16 @@
+import Image from 'next/image';
 import { useTranslation } from "next-i18next";
+import { useDeviceSize } from "../../../common/hooks";
+import TaskImage from "../../../public/images/img-chelzoo-dev-design.jpg";
 
 export function ChelzooDevDesign() {
   const {
     t,
   } = useTranslation(`chelzooDevDesign`);
+
+  const {
+    isMobile,
+  } = useDeviceSize();
 
   return (
     <section
@@ -12,11 +19,34 @@ export function ChelzooDevDesign() {
     >
       <div className="container-cases chelzoo-dev-design__wrapper">
         <h2 className="chelzoo-dev-design__title">
-          {t(`title`)}
+          {isMobile ? t(`titleMobile`) : t(`title`)}
+          {` `}
+          <span
+            className="chelzoo-dev-design__heart"
+          >
+            ❤
+          </span>
         </h2>
         <p className="chelzoo-dev-design__description">
           {t(`description`)}
         </p>
+        <div className="chelzoo-dev-design__image-wrapper">
+          <Image
+            className="chelzoo-dev-design__image"
+            src={TaskImage}
+            alt=""
+            fill
+            placeholder="blur"
+          />
+        </div>
+        <a
+          className="chelzoo-dev-design__link"
+          href={t(`link`)}
+        >
+          {t(`linkText`)}
+          {` `}
+          →
+        </a>
       </div>
     </section>
   );
