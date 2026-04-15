935990f0dbc36451088f64518846a3d257a5cb4d
Ekaterina <echekina@gmail.com>
2026-04-15 17:25:03 +0500

    style: #74: component styling

--- ui/components/chelzoo/ChelzooAutotests/ChelzooAutotests.tsx
+++ ui/components/chelzoo/ChelzooAutotests/ChelzooAutotests.tsx
@@ -1,4 +1,8 @@
 import { useTranslation } from 'next-i18next';
+import Image from 'next/image';
+import ArrowCenter from '../../../public/images/chelzoo-arrow-center.png';
+import ArrowLeft from '../../../public/images/chelzoo-arrow-left.png';
+import ArrowRight from '../../../public/images/chelzoo-arrow-right.png';
 
 type TestingTypes = {
   title: string;
@@ -27,13 +31,13 @@ export function ChelzooAutotests() {
           {testingTypes.map(({
             title,
             text,
-          }) => (
+          }, index) => (
             <li
               key={title}
               className="chelzoo-autotests__item"
             >
               <div className="chelzoo-autotests__content">
-
+                <span className="chelzoo-autotests__step">{index + 1}</span>
                 <h3 className="chelzoo-autotests__subtitle">
                   {title}
                 </h3>
@@ -44,6 +48,23 @@ export function ChelzooAutotests() {
             </li>
           ))}
         </ul>
+        <div className="chelzoo-autotests__arrows">
+          <Image
+            className="chelzoo-autotests__arrow chelzoo-autotests__arrow--left"
+            src={ArrowLeft}
+            alt=""
+          />
+          <Image
+            className="chelzoo-autotests__arrow chelzoo-autotests__arrow--center"
+            src={ArrowCenter}
+            alt=""
+          />
+          <Image
+            className="chelzoo-autotests__arrow chelzoo-autotests__arrow--right"
+            src={ArrowRight}
+            alt=""
+          />
+        </div>
         <div className="chelzoo-autotests__result">
           {t(`result`)}
         </div>
