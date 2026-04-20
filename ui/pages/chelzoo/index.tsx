2ccf324cd67c2f4eb475bed90e4695db6c115c60
Anastasia Saraeva <asaraeva@tourmalinecore.com>
2026-04-20 14:35:21 +0500

    feat: #74: add collage component

--- ui/pages/chelzoo/index.tsx
+++ ui/pages/chelzoo/index.tsx
@@ -2,7 +2,7 @@ import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import { useTranslation } from 'next-i18next';
 import { PageHead } from '../../components/PageHead/PageHead';
 import { useScrollTop } from '../../common/hooks/useScrollTop';
-import { LayoutData } from '../../common/types';
+import { CollageWithLinkBlock, LayoutData } from '../../common/types';
 import { getLayoutData } from '../../services/cms/api/layout-api/layout-api';
 import { loadTranslations } from '../../common/utils';
 import { LayoutRedesign } from '../../components/redesign/LayoutRedesign/LayoutRedesign';
@@ -24,12 +24,15 @@ import { ChelzooInfrastructure } from '../../components/chelzoo/ChelzooInfrastru
 import { ChelzooCMS } from '../../components/chelzoo/ChelzooCMS/ChelzooCMS';
 import { ChelzooPromo } from '../../components/chelzoo/ChelzooPromo/ChelzooPromo';
 import { ChelzooReview } from '../../components/chelzoo/ChelzooReview/ChelzooReview';
+import { CollageWithLink } from '../../components/CollageWithLink/CollageWithLink';
 
 export default function ChelzooPage({
   layoutData,
+  collageWithLinkData,
   isPreview,
 }: {
   layoutData: LayoutData;
+  collageWithLinkData: CollageWithLinkBlock;
   isPreview: boolean;
 }) {
   const {
@@ -79,6 +82,11 @@ export default function ChelzooPage({
           <ChelzooAutotests />
           <ChelzooReview />
           <ChelzooPromo />
+          <CollageWithLink
+            text={collageWithLinkData.text}
+            link={collageWithLinkData.link}
+            imagesWithBlurDataURL={collageWithLinkData.imagesWithBlurDataURL}
+          />
         </div>
       </LayoutRedesign>
     </>
@@ -93,13 +101,22 @@ export async function getServerSideProps({
   preview: boolean;
 }) {
   if (process.env.IS_STATIC_MODE === `true`) {
-    const translationsPageData = await loadTranslations(locale, [`headerRedesign`, `footerRedesign`]);
+    const translationsPageData = await loadTranslations(locale, [
+      `headerRedesign`,
+      `footerRedesign`,
+      `collageWithLink`,
+    ]);
 
     return {
       props: {
         layoutData: {
           headerContent: translationsPageData.headerRedesign,
           footerContent: translationsPageData.footerRedesign,
+        },
+        collageWithLinkData: {
+          text: translationsPageData.collageWithLink.text,
+          link: translationsPageData.collageWithLink.link,
+          imagesWithBlurDataURL: translationsPageData.collageWithLink.imagesWithBlurDataURL,
         },
         ...(await getStaticTranslation({
           locale,
