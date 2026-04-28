c63b958a61825c67f09a475af7289e62067aeecb
Ekaterina <echekina@gmail.com>
2026-04-28 14:35:48 +0500

    feat(UI): #80: style cookie settings modal

--- ui/components/CookieSettingsModal/CookieSettingsModal.tsx
+++ ui/components/CookieSettingsModal/CookieSettingsModal.tsx
@@ -1,6 +1,7 @@
 import { useTranslation } from 'next-i18next';
 import clsx from 'clsx';
 import { Modal } from '../Modal/Modal';
+import { useDeviceSize } from '../../common/hooks';
 
 type OptionsList = {
   title: string;
@@ -22,48 +23,65 @@ export function CookieSettingsModal({
     returnObjects: true,
   });
 
+  const {
+    isTablet,
+  } = useDeviceSize();
+
   return (
-    <Modal
-      className={clsx(`cookie-settings-modal`, {
-        'cookie-settings-modal--open': isModalOpen,
-      })}
-      data-testid="cookie-settings-modal"
-      onClose={onCloseModal}
-    >
-      <div
-        className="cookie-settings-modal__inner"
+    <>
+      {isModalOpen && isTablet && <div className="cookie-settings-modal-overlay" />}
+      <Modal
+        className={clsx(`cookie-settings-modal modal--cookie-settings`, {
+          'cookie-settings-modal--open': isModalOpen,
+        })}
+        data-testid="cookie-settings-modal"
+        onClose={onCloseModal}
       >
-        <h2 className="cookie-settings-modal__title">{t(`title`)}</h2>
-        <ul className="cookie-settings-modal__list">
-          {optionsList.map(({
-            text, title,
-          }) => (
-            <li
-              key={title}
-              className="cookie-settings-modal__item"
-            >
-              <span
-                className="cookie-settings-modal__radio"
-              />
-              <h3>{t(`title`)}</h3>
-              <span
-                className="cookie-settings-modal__description"
+        <div className="cookie-settings-modal__inner">
+          <h2 className="cookie-settings-modal__title">{t(`title`)}</h2>
+          <ul className="cookie-settings-modal__list">
+            {optionsList.map(({
+              text, title,
+            }) => (
+              <li
+                key={title}
+                className="cookie-settings-modal__item"
               >
-                {text}
-              </span>
-            </li>
-          ))}
-        </ul>
-        <div>{t(`note`)}</div>
-        <button
-          type="button"
-          className="cookie-settings-modal__button"
-          // onClick={}
-          data-testid="save-cookie-settings-button"
-        >
-          {t(`buttonText`)}
-        </button>
-      </div>
-    </Modal>
+                <div className="cookie-settings-modal__option">
+                  <div className="cookie-settings-modal__checkbox">
+                    <input
+                      onChange={() => {}}
+                      type="checkbox"
+                      className="cookie-settings-modal__checkbox-input"
+                      value=""
+                      id={title}
+                    />
+                    <div className="cookie-settings-modal__checkbox-indicator" />
+                  </div>
+                  <label
+                    className="cookie-settings-modal__label"
+                    htmlFor={title}
+                  >
+                    {title}
+                  </label>
+                </div>
+                <p className="cookie-settings-modal__description">
+                  {text}
+                </p>
+              </li>
+            ))}
+          </ul>
+          <div className="cookie-settings-modal__note">{t(`note`)}</div>
+          <button
+            type="button"
+            className="cookie-settings-modal__button"
+            // onClick={}
+            data-testid="save-cookie-settings-button"
+          >
+            {t(`buttonText`)}
+          </button>
+        </div>
+      </Modal>
+    </>
   );
 }
