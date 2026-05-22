d8cdebd516ad01ad6d8f36b8606d6b1c3c18ceb3
Dmitriy Myakotin <75628188+MDI74@users.noreply.github.com>
2026-05-22 15:33:29 +0500

    test(CMS): #80: add api test for cookie consent collection

---
+++ cms/playwright-tests/api-tests/cookie-consent-collection/cookie-consent-collection-api.ts
@@ -0,0 +1,72 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+import { HttpStatusCode } from "../../enums";
+import { ApiTestFixtures, expect } from "../../api-test-fixtures";
+import { API_SMOKE_NAME_PREFIX } from "../../constants";
+
+export const COOKIE_CONSENTS_ENDPOINT = `/api/cookie-consents`;
+
+export async function createCookieConsent({
+  apiRequest
+}: {
+  apiRequest: ApiTestFixtures[`apiRequest`];
+}) {
+  try {
+    const response = await apiRequest(COOKIE_CONSENTS_ENDPOINT, {
+      method: `post`,
+      data: {
+        data: {
+          consentId: `${API_SMOKE_NAME_PREFIX} Consent Id`,
+          consentVersion: `Analytics text`,
+          categories: {
+            analytics: false,
+            webvisor: true
+          },
+        },
+      }
+    });
+
+    await expect(response.status(), `Cookie consent should be updated with status 200`)
+      .toEqual(HttpStatusCode.Created);
+  } catch (error: any) {
+    throw new Error(`Failed to update test cookie consent: ${error.message}`)
+  }
+}
+
+
+export async function cleanupCookieConsent({
+  apiRequest
+}: {
+  apiRequest: ApiTestFixtures[`apiRequest`];
+}) {
+  try {
+    const cookieConsentList = await getCookieConsentData({
+      apiRequest
+    });
+
+    const cookieConsentToDelete = cookieConsentList?.data
+      ?.filter((cookieConsent: { consentId: string }) => cookieConsent.consentId.startsWith(API_SMOKE_NAME_PREFIX));
+
+    cookieConsentToDelete?.forEach(async (cookieConsent: { documentId: string }) => {
+      const response = await apiRequest(`${COOKIE_CONSENTS_ENDPOINT}/${cookieConsent.documentId}`, {
+        method: `delete`,
+      });
+
+      await expect(response.status, `Cookie consent should be deleted with status 204`)
+        .toEqual(204);
+    });
+  } catch (error: any) {
+    throw new Error(`Failed to delete test cookie consent: ${error.message}`)
+  }
+}
+
+export async function getCookieConsentData({
+  apiRequest
+}: {
+  apiRequest: ApiTestFixtures[`apiRequest`];
+}) {
+  const response = await apiRequest(`${COOKIE_CONSENTS_ENDPOINT}?populate=all`);
+  const responseData = await response.json();
+
+  return responseData.data;
+}
---
+++ cms/playwright-tests/api-tests/cookie-consent-collection/cookie-consent-collection.spec.ts
@@ -0,0 +1,59 @@
+import z from "zod";
+import { ApiTestFixtures, expect, test } from "../../api-test-fixtures";
+import { cleanupCookieConsent, COOKIE_CONSENTS_ENDPOINT, createCookieConsent, getCookieConsentData } from "./cookie-consent-collection-api";
+
+const CookieConsentSchema = z.array(
+  z.object({
+    consentId: z.string(),
+    consentVersion: z.string(),
+    categories: z.record(z.string(), z.boolean())
+  }));
+
+test.describe(`Cookie consent response tests`, () => {
+  test.beforeEach(async ({
+    apiRequest 
+  }) => {
+    await cleanupCookieConsent({
+      apiRequest 
+    });
+
+    await createCookieConsent({
+      apiRequest 
+    });
+  });
+
+  test.afterEach(async ({
+    apiRequest 
+  }) => {
+    await cleanupCookieConsent({
+      apiRequest 
+    });
+  });
+
+  test(`
+      GIVEN an empty cookie consent collection
+      WHEN call method POST ${COOKIE_CONSENTS_ENDPOINT}
+      AND call method GET ${COOKIE_CONSENTS_ENDPOINT}
+      SHOULD get a correct response
+      `,
+  checkCookieConsentResponseTest
+  );
+});
+
+async function checkCookieConsentResponseTest({
+  apiRequest
+}: {
+  apiRequest: ApiTestFixtures[`apiRequest`];
+}) {
+  const cookieConsent = await getCookieConsentData({
+    apiRequest
+  });
+
+  console.log(cookieConsent)
+  
+  await expect(() => {
+    CookieConsentSchema.parse(cookieConsent)
+  }, `Cookie consent response is correct`)
+    .not
+    .toThrow()
+}
