d5f1eb880399748da578c0e6fa25a8365aad94b8
Maxim Rychkov <rychkov03@yandex.ru>
2026-07-31 11:40:30 +0500

    feat(env-synchronizer): #97: use subprocess.Popen instead of subprocess.run to get access to the process logs and add timestamps to them and add new logs for each step

--- env-synchronizer/synchronizer.py
+++ env-synchronizer/synchronizer.py
@@ -1,14 +1,43 @@
 import subprocess
 import os
+import logging
+
+def run(cmd):
+    process = subprocess.Popen(
+        cmd,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+        text=True
+    )
+
+    for line in process.stdout:
+        # rstrip() removes spaces and line break characters
+        logging.info(line.rstrip())
+
+    # handling error termination
+    if process.wait() != 0:
+        raise subprocess.CalledProcessError(process.wait(), cmd)
+
+# Example of log: [2026-07-31T06-21-44]: Helmfile configuration applied successfully
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s]: %(message)s",
+    datefmt="%Y-%m-%dT%H-%M-%S",
+)
+
 
 if (os.getenv("SYNCHRONIZER_ENABLED") == "true"):
-    # By default subprocess.run doesn't throw error if the command was failed due runtime and pod completes without any errors
-    # with check = True subprocess.run will throw error and pod will be completed with error
-    subprocess.run(['git', 'clone', f'https://github.com/{os.getenv("ENV_REPOSITORY")}.git'], check = True)
+    logging.info(f'Cloning repository {os.getenv("ENV_REPOSITORY")}')
+    run(["git", "clone", f'https://github.com/{os.getenv("ENV_REPOSITORY")}.git', "--progress"])
+    logging.info(f'Repository {os.getenv("ENV_REPOSITORY")} cloned successfully')
 
-    subprocess.run(['helmfile', 'cache', 'cleanup'], check = True)
+    logging.info('Cleaning up helmfile cache')
+    run(['helmfile', 'cache', 'cleanup'])
+    logging.info('Helmfile cache cleaned')
 
-    subprocess.run(['helmfile', '--environment', f'{os.getenv("NAMESPACE")}', '--namespace', f'{os.getenv("NAMESPACE")}', '-f', f'{os.getenv("PATH_TO_HELMFILE")}', 'apply', '--concurrency', '1'], check = True)
+    logging.info('Applying new configuration')
+    run(['helmfile', '--environment', f'{os.getenv("NAMESPACE")}', '--namespace', f'{os.getenv("NAMESPACE")}', '-f', f'{os.getenv("PATH_TO_HELMFILE")}', 'apply', '--concurrency', '1'])
+    logging.info('Helmfile configuration applied successfully')
 
 else:
-    print("SYNCHRONIZER IS DISABLED")
+    logging.info('SYNCHRONIZER IS DISABLED')
