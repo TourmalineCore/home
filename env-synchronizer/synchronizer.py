e08da8c2c47a25ad9e813781bae678fc66941fe9
Maxim Rychkov <rychkov03@yandex.ru>
2026-08-11 16:42:56 +0500

    chore: move global logging config definition to the start of code and add comms for commands

--- env-synchronizer/synchronizer.py
+++ env-synchronizer/synchronizer.py
@@ -2,8 +2,15 @@ import subprocess
 import os
 import logging
 
+# Example of log: [2026-07-31T06-21-44]: Helmfile configuration applied successfully
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s]: %(message)s",
+    datefmt="%Y-%m-%dT%H-%M-%S",
+)
+
 def run(cmd):
-    # Popen is used to read command output line by line
+    # Popen is used to read command output line by line. We used `subprocess.run`, which doesn't allow to print logs line by line
     process = subprocess.Popen(
         cmd,
         stdout=subprocess.PIPE,
@@ -12,19 +19,12 @@ def run(cmd):
     )
 
     for line in process.stdout:
-        # rstrip() removes spaces and line break characters
+        # rstrip() removes spaces and line break characters. Without this step there might be a situation when end of line contains `\n` or `\r` symbols
         logging.info(line.rstrip())
 
-    # handling error termination
+    # Handling error termination. Without this step command will be recognized as successfull even if it finished with an error. 
     if process.wait() != 0:
         raise subprocess.CalledProcessError(process.wait(), cmd)
-
-# Example of log: [2026-07-31T06-21-44]: Helmfile configuration applied successfully
-logging.basicConfig(
-    level=logging.INFO,
-    format="[%(asctime)s]: %(message)s",
-    datefmt="%Y-%m-%dT%H-%M-%S",
-)
 
 
 if (os.getenv("SYNCHRONIZER_ENABLED") == "true"):
@@ -37,6 +37,7 @@ if (os.getenv("SYNCHRONIZER_ENABLED") == "true"):
     logging.info('Helmfile cache cleaned')
 
     logging.info('Applying new configuration')
+    # The --concurrency 1 flag was used to process only one service at a time, thereby consuming fewer resources.
     run(['helmfile', '--environment', f'{os.getenv("NAMESPACE")}', '--namespace', f'{os.getenv("NAMESPACE")}', '-f', f'{os.getenv("PATH_TO_HELMFILE")}', 'apply', '--concurrency', '1'])
     logging.info('Helmfile configuration applied successfully')
 
